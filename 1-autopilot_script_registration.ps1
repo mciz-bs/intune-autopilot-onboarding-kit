da101218478b60e4bbe95ad841f0926fcdbcf9df
mciz-bs <martin.ciz@gmail.com>
2025-09-08 16:53:46 +0200

    Update 1-autopilot_script_registration.ps1
    For GAK

--- 1-autopilot_script_registration.ps1
+++ 1-autopilot_script_registration.ps1
@@ -1,4 +1,10 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
- Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
- Install-Script -Name Get-WindowsAutopilotInfo -Force
- Get-WindowsAutopilotInfo -Online
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-Script -Name Get-WindowsAutopilotInfo -Force
+
+# Vytvor priečinok, ak neexistuje
+New-Item -Path "C:\Autopilot" -ItemType Directory -Force
+Set-Location "C:\Autopilot"
+
+# Spusti skript s výstupom do CSV a Group Tag
+Get-WindowsAutopilotInfo -OutputFile "AutopilotHWID.csv" -GroupTag "student-hp"
